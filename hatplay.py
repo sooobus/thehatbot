8848631898906bdd67a0b9d570bf281d4c04847b
Valeriia Nemychnikova <sooobus@gmail.com>
2017-04-30 19:35:52 +0300

    added words storage and circle play; no connection to TG yet

--- hatplay.py
+++ hatplay.py
@@ -1,7 +1,10 @@
 import random
+from collections import deque
+import time
 
 class Circle(object):
     def __init__(self, players_number, is_pair=False, players_names=[], circle_limit=None, words_limit=None):
+    #TODO uniq names
         assert(1 < players_number <= 20)
         self.players = players_names
         if len(players_names) < players_number:
@@ -11,13 +14,64 @@ class Circle(object):
         self.is_pair = is_pair
         self.circle_limit = circle_limit
         self.words_limit = words_limit
-        self.hat = ...
+        self.w_storage = WordsStorage(config.storage_filename)
 
     def show(self):
         print(len(self.players))
         print(self.players)
 
+#на каждом круге обновляем список партнёров
+    def individual_play(self):
 
+        explained = {}
+        def add_explained(word, time, finished):
+            if word in explained:
+                explained[word]['time'] += time
+            else:
+                explained[word]['time'] = time
+            explained[word]['finished'] = finished
 
-c = Circle(5)
-c.show()
+        def make_turn():
+             print(player_names[ind] + " объясняет к " player_names[(ind + shift) % players_num])
+
+            timer.start()
+            word = hat.pop()
+            t0 = time.clock()
+            pre = time.clock()
+            cnt = 0
+            print(word)
+            while(time.clock() - t0 < 20.0):
+                sleep(0.5)
+                if check_status():
+                    cnt += 1
+                    add_explained(word, time.clock() - pre, True) 
+                    word = hat.pop()
+                    pre = time.clock()
+                    print(word)
+            print("Время!")
+            sleep(3)
+                if check_status():
+                    cnt += 1
+                    add_explained(word, time.clock() - pre)
+            return cnt
+#TODO апель
+        players_num = len(self.players)
+        count = [[ 0 ] * players_num] * players_num
+        if self.circle_limit:
+            self.words_limit = cicle_limit * players_num * 6
+        hat = deque(self.w_storage.take_hat(self.words_limit))
+        shift = 1
+        circle_cnt = 0
+        expl_cnt = 0
+        while circle_cnt < self.cicle_limit and words_queue.size()
+            ind = expl_cnt % players_num
+            count[ind][(ind + shift) % players_num] += make_turn()
+            expl_cnt += 1
+            if expl_cnt % players_num == 0:
+                cicle_cnt += 1
+                shift += 1
+                shift %= players_num
+                if shift == 0:
+                    shift += 1
+
+#game = Circle(circle_limit=10
--- words.py
+++ words.py
@@ -1,18 +1,58 @@
 import shelve
 import pandas as pd
+import math
 
-class WordsStorage(object):
+class WordsStorage(object): #this class is used to interact with the storage of evaluated words 
     def __init__(self, filename):
         self.storage = shelve.open(filename)
     def __exit__(self):
         self.storage.close()
 
-    def add_word(word, complexity=None):
-        self.storage[word] = (1 / (13 - complexity))
+    def add_word(self, word, complexity):
+        if word in self.storage:
+            self.storage[word] = {'human': (1 / (13 - complexity), 'time': [], 'complexity': (1 / (13 - complexity))
 
-    def add_from_csv(filename):
+    def add_from_csv(self, filename):
         words = pd.read_csv("sample_csv.csv", header=None, names=['word', 'complexity'], delimiter=' ')
         print(words.head())    
         for word in words['word']:
             add_word(word, [words[words['word'] == word]['complexity'])
 
+    def add_time(self, word, time):
+        time = min(time, 60.0) #if the word was explained more than 3 turns, it is possibly not so easy
+        if word in self.storage:
+            self.storage[word]['time'].append(time)
+            time = self.storage[word]['time']
+            meantime = sum(time) / len(time) 
+            normal_time = math.tanh(meantime / 30)
+            l = min(10, len(time))
+            self.storage[word]['complexity'] = self.storage[word]['human'] * (1 - 0.1 * l) + normal_time * (0.1 * l)
+
+    def take_hat(size, complexity_lower, complexity_upper):
+        hat = [word for word, params in self.storage if complexity_upper <= params['complexity'] < complexity_lower]
+        if len(hat) > size:
+            return random.sample(hat, size)
+        else:
+            return random.shuffle(hat)
+
+class Reviewer(object): #this class is used to interact with not yet evaluated words
+    def __init__(self, filename):
+        self.storage = shelve.open(filename)
+
+    def __exit__(self):
+        self.storage.close()
+
+    def add_word(word): # storage stores pairs (word, list), where list contains all the marks for the word
+        if word not in self.storage:
+            self.storage[word] = []
+
+    def add_mark(word, mark):
+        if word in self.storage and 0 <= mark <= 12 and len(self.storage) < 3:
+            self.storage[word].append(mark)
+
+    def transit_evaluated(filename): #moves words with 3 marks to playable
+        s = WordsStorage(filename)
+        for word in self.storage:
+            if len(self.storage[word]) == 3:
+                complexity = sum(self.storage[word])
+                s.add_word(word, complexity)
